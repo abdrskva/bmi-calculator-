6b148e71a865c47742b1458a52f8f21dc9e21179
abdrskva <152314649+abdrskva@users.noreply.github.com>
2024-11-15 14:58:19 +0600

    Delete BMIcalculator directory

--- BMIcalculator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.bmicalculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.bmicalculator to javafx.fxml;
-    exports org.example.bmicalculator;
-}
